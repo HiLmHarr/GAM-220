ac0ede16365b6cdabc3d259841fca9ff4aef0389
HiLmHarr <harrisonisaiahlasyone@gmail.com>
2024-02-01 02:44:24 -0500

    String manipulation finished
    1 major bug if a space is put in the string inputs or a non integer is put in the integer inputs

--- Additional String Manipulation/Additional String Manipulation.cpp
+++ Additional String Manipulation/Additional String Manipulation.cpp
@@ -1,5 +1,5 @@
-// Additional String Manipulation.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//MAJOR BUG: Choice is set to 0 and cannot be changed so the project runs in an infinite loop until closed
+//This is caused either by inputing something that is not an integer for integer inputs or by putting a space in the string input
 
 #include <iostream>
 #include <string>
@@ -21,6 +21,8 @@ bool BadInput(string myString)
 int main()
 {
     int choice = 0; //Had to make this an int as Switch statements don't work with strings
+    int firstIndex;
+    int secondIndex;
     string myString = "";
     string extraString; //This just makes some of the options easier
     bool wrong = false;
@@ -30,13 +32,14 @@ int main()
             << "4. reverse string" << endl << "5. Captialize all letters" << endl << "6. lowercase all letters" << endl <<
             "7. Toggle Capitalization for characters at index" << endl << "8. swap characters at 2 indicies" << endl <<
             "9. Captitalize the first letter of every word" << endl << "10. quit" << endl << "> ";
-        cin >> choice;
-
+        
+            cin >> choice;
+        
         switch (choice) {
-        case 1:
+        case 1: //outputs string
             cout << myString << endl;
             break;
-        case 2:
+        case 2: //replaces string with input
             cout << "Input text" << endl << "> ";
             cin >> extraString;
             wrong = BadInput(extraString);
@@ -44,34 +47,99 @@ int main()
             {
                 cout << "Bad Input, please use only letters" << endl;
                 extraString = "";
-                wrong = false;
             }
             else
             {
                 myString = extraString;
             }
             break;
-        case 3:
-            getline(cin, extraString);
+        case 3: //adds input onto existing string
+            cin >> extraString;
             wrong = BadInput(extraString);
             if (wrong)
             {
                 cout << "Bad Input, please use only letters" << endl;
                 extraString = "";
-                wrong = false;
             }
             else
             {
                 myString = myString + extraString;
             }
             break;
-        case 4:
+        case 4: //reverses letter order
+            //I genuinely don't know my thought process for this but it works
+            for (int i = 0; i < myString.length(); i++) 
+            {
+                myString = myString + myString[myString.length() - (i + 1)];
+                myString.erase(myString.length() - (i + 2), 1);
+            }
             break;
-        default:
-            cout << "Input not recognized, please use the number preceading the choice" << endl;
+        case 5: //capitalizes all lowercase letters
+            for (int i = 0; i < myString.length(); i++) 
+            {
+                if (myString[i] > 96 && myString[i] < 123) 
+                {
+                    myString[i] -= 32;
+                }
+            }
+            break;
+        case 6: //lowercases all capital letters
+            for (int i = 0; i < myString.length(); i++)
+            {
+                if (myString[i] > 64 && myString[i] < 91)
+                {
+                    myString[i] += 32;
+                }
+            }
+            break;
+        case 7: //Capitalizes all letters after index
+            cout << "Capitalize after what index?" << endl << "> ";
+            cin >> firstIndex;
+            if (firstIndex < 0 || firstIndex > myString.length()) //check to make index is in bounds
+            {
+                cout << "invalid index" << endl;
+                break;
+            }
+            for (int i = firstIndex; i < myString.length(); i++) 
+            {
+                if (myString[i] > 96 && myString[i] < 123)
+                {
+                    myString[i] -= 32;
+                }
+            }
+            break;
+        case 8: //swaps 2 characters at indexes
+            cout << "First index" << endl << "> ";
+            cin >> firstIndex;
+            cout << "Second index" << endl << "> ";
+            cin >> secondIndex;
+            if (firstIndex < 0 || firstIndex > myString.length() || secondIndex < 0 || secondIndex > myString.length())
+            {
+                cout << "invalid index" << endl;
+                break;
+            }
+            extraString = myString[firstIndex];
+            myString[firstIndex] = myString[secondIndex];
+            myString[secondIndex] = extraString[0];
+            extraString = "";
+            break;
+        case 9: //capitalizes characters after a space
+            for (int i = 0; i < myString.length(); i++) 
+            {
+                if (myString[i] = 32 && i < myString.length() - 1) 
+                {
+                    if (myString[i + 1] > 96 && myString[i + 1] < 123)
+                    {
+                        myString[i + 1] -= 32;
+                    }
+                }
+            }
+            break;
+        default: //just in case they don't input a proper number
+            cout << "invalid input, please use the number preceding the choice" << endl;
             break;
         }
-        cout << endl << endl;
+        cout << endl;
     }
 }
 
