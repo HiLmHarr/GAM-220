574a3a40f797430b073295cfb90a035b243ebb16
HiLmHarr <harrisonisaiahlasyone@gmail.com>
2024-02-28 21:31:27 -0500

    ?

---
+++ Quiz practice/Quiz practice.cpp
@@ -0,0 +1,12 @@
+// Quiz practice.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int x = 0;
+    int* ptr = &x;
+    std::cout << ptr;
+}
+
